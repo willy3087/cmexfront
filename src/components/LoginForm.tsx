5da592f4613100e518ed525aea7af4aa9528e1bf
willy3087 <william.duarte@muralis.com.br>
2024-12-02 14:08:40 -0300

    Atualizar URL da API no componente LoginForm

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -25,7 +25,7 @@ const LoginForm: React.FC = () => {
     setResetTime(false);
     try {
       const response = await fetch(
-        "https://cmexback-williams-projects-2c392421.vercel.app/api/index",
+        "https://cmexback-i617kfvd5-williams-projects-2c392421.vercel.app/api/index",
         {
           method: "POST",
           headers: {
