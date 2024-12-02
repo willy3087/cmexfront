720d99470a812376758edc321302542d8f02a49f
willy3087 <william.duarte@muralis.com.br>
2024-12-02 13:23:20 -0300

    Adicionar configuração de base para o projeto Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/willy3087/cmexfront/",
   plugins: [react()],
   server: {
     open: true, // Adiciona esta linha para abrir o navegador automaticamente
