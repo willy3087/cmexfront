f806c0dddf2417e45b9ef84e9cb543f1969316a6
willy3087 <william.duarte@muralis.com.br>
2024-12-03 16:25:45 -0300

    Atualizar URL base do Axios para HTTP e adicionar configuração de base no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/cmexfront/", // Substitua 'nome-do-repositorio' pelo nome do seu repositório
   plugins: [react()],
   server: {
     open: true,
